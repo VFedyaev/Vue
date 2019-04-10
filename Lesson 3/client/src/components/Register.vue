b4a3c534207be483dc084f2d04e3154e66a42b2c
VFedyaev <VFedyaev@stat.kg>
2019-04-10 17:20:39 +0600

    authorization

---
+++ Lesson 3/client/src/components/Register.vue
@@ -0,0 +1,43 @@
+<template>
+<div>
+<h1>Register</h1>
+
+<input type = "email" name = "email" v-model="email" placeholder = "email" />
+<input type = "password" name = "password" v-model="password" placeholder = "password" />
+<br>
+<button @click = "register">Register</button>
+</div>
+</template>
+
+<script>
+import AuthenticationService from '@/services/AuthenticationService'
+export default {
+  data () {
+    return {
+      email: '',
+      password: ''
+    }
+  },
+  methods: {
+    async register () {
+      await AuthenticationService.register({
+        email: this.email,
+        password: this.password
+      })
+    }
+  }
+  // watch: {
+  //   email (value) {
+  //     console.log('email has changed', value)
+  //   }
+  // },
+  // mounted (){
+  //   setTimeout(() => {
+  //     this.email = 'hello world'
+  //   }, 2000);
+  // }
+}
+</script>
+
+<style scoped>
+</style>
