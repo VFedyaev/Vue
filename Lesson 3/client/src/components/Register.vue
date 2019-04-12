570b924c67ae47da6f383457bd5da48a2695acca
VFedyaev <VFedyaev@stat.kg>
2019-04-12 17:34:09 +0600

    authorize

--- Lesson 3/client/src/components/Register.vue
+++ Lesson 3/client/src/components/Register.vue
@@ -1,12 +1,34 @@
 <template>
-<div>
-<h1>Register</h1>
-
-<input type = "email" name = "email" v-model="email" placeholder = "email" />
-<input type = "password" name = "password" v-model="password" placeholder = "password" />
-<br>
-<button @click = "register">Register</button>
-</div>
+  <v-layout column>
+    <v-flex xs6 offset-xs3>
+      <panel title="Register">
+        <form
+          name="tab-tracker-form"
+          autocomplete="off">
+          <v-text-field
+            label="Email"
+            v-model="email"
+          ></v-text-field>
+          <br>
+          <v-text-field
+            label="Password"
+            type="password"
+            v-model="password"
+            autocomplete="new-password"
+          ></v-text-field>
+        </form>
+        <br>
+        <div class="danger-alert" v-html="error" />
+        <br>
+        <v-btn
+          dark
+          class="cyan"
+          @click="register">
+          Register
+        </v-btn>
+      </panel>
+    </v-flex>
+  </v-layout>
 </template>
 
 <script>
@@ -15,15 +37,20 @@ export default {
   data () {
     return {
       email: '',
-      password: ''
+      password: '',
+      error: null
     }
   },
   methods: {
     async register () {
-      await AuthenticationService.register({
-        email: this.email,
-        password: this.password
-      })
+      try {
+        await AuthenticationService.register({
+          email: this.email,
+          password: this.password
+        })
+      } catch (error) {
+        this.error = error.response.data.error
+      }
     }
   }
   // watch: {
