7478290118e1e983d7a2a1cb980329644a11d36f
VFedyaev <VFedyaev@stat.kg>
2019-04-15 17:36:13 +0600

    crud songs

--- Lesson 3/client/src/components/Header.vue
+++ Lesson 3/client/src/components/Header.vue
@@ -1,11 +1,17 @@
 <template>
   <v-toolbar fixed class="cyan" dark>
     <v-toolbar-title class="mr-4">
-        TabTracker
+        <span
+            class="home"
+            @click="navigateTo({name: 'root'})">
+            TabTracker
+        </span>
     </v-toolbar-title>
 
     <v-toolbar-items>
-        <v-btn flat dark>
+        <v-btn
+            flat dark
+            @click="navigateTo({name: 'songs'})">
             Browse
         </v-btn>
     </v-toolbar-items>
@@ -13,8 +19,25 @@
     <v-spacer></v-spacer>
 
     <v-toolbar-items>
-        <v-btn flat dark>
+        <v-btn
+        v-if="!$store.state.isUserLoggedIn"
+        flat dark
+        @click="navigateTo({name: 'login'})">
+            Login
+        </v-btn>
+
+        <v-btn
+        v-if="!$store.state.isUserLoggedIn"
+        flat dark
+        @click="navigateTo({name: 'register'})">
             Sign Up
+        </v-btn>
+
+        <v-btn
+        v-if="$store.state.isUserLoggedIn"
+        flat dark
+        @click="logout">
+            Log Out
         </v-btn>
     </v-toolbar-items>
 
@@ -22,8 +45,25 @@
 </template>
 
 <script>
-export default {}
+export default {
+  methods: {
+    navigateTo (route) {
+      this.$router.push(route)
+    },
+    logout () {
+      this.$store.dispatch('setToken', null)
+      this.$store.dispatch('setUser', null)
+
+      this.$router.push({
+        name: 'root'
+      })
+    }
+  }
+}
 </script>
 
 <style scoped>
+.home {
+    cursor: pointer;
+}
 </style>
